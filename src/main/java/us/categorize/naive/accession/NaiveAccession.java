8cf7312833a1b03789f5ae727c239f839c999ee4
keefe <keefe@categorize.us>
2019-01-06 10:36:54 -0800

    streamline configuration

--- src/main/java/us/categorize/naive/accession/NaiveAccession.java
+++ src/main/java/us/categorize/naive/accession/NaiveAccession.java
@@ -1,25 +1,32 @@
 package us.categorize.naive.accession;
 
+import java.io.InputStream;
+import java.util.Properties;
 import java.util.UUID;
 
-import us.categorize.Configuration;
+import us.categorize.CategorizeUs;
 import us.categorize.api.UserStore;
 import us.categorize.model.User;
 import us.categorize.naive.NaiveMessageStore;
 import us.categorize.naive.NaiveUserStore;
 import us.categorize.naive.accession.domains.Reddit;
 import us.categorize.naive.api.NaiveAuthorizer;
+import us.categorize.naive.app.Config;
+import us.categorize.naive.app.NaiveApp;
 
 public class NaiveAccession {
 	private static final String userName = "reddit-user";
 	private static final String pass = "35789fb6e";
 	
 	public static void main(String[] args) throws Exception{
-		Config config = Config.readRelativeConfig();
-		Configuration.instance().setUserStore(new NaiveUserStore(config.getDatabaseConnection()));
-		Configuration.instance().setMessageStore(new NaiveMessageStore(config.getDatabaseConnection(), Configuration.instance().getUserStore(), config.getFileBase()));
-		Configuration.instance().setAuthorizer(new NaiveAuthorizer(Configuration.instance().getUserStore()));
-		UserStore userStore = Configuration.instance().getUserStore();
+		Properties properties = new Properties();
+		InputStream input = NaiveApp.class.getResourceAsStream("/categorizeus.properties");
+		properties.load(input);
+		//note overrideProperties and toLocal in Config, just load another properties files and override with these as desired
+		Config config = new Config(properties);
+		config.configureCategorizeUs();
+		
+		UserStore userStore = CategorizeUs.instance().getUserStore();
 		
 		User user = new User();
 		user.setUsername(userName);
@@ -32,7 +39,7 @@ public class NaiveAccession {
 		String sessionKey = UUID.randomUUID().toString();
 		if(!userStore.validateUser(user)) {
 			user.setPasshash(NaiveUserStore.sha256hash(NaiveUserStore.sha256hash(pass)));
-			Configuration.instance().getUserStore().registerUser(user);
+			CategorizeUs.instance().getUserStore().registerUser(user);
 			user.setPasshash(NaiveUserStore.sha256hash(pass));
 			if(!userStore.establishUserSession(user, sessionKey)) {
 				throw new RuntimeException("Unable to create user session");
@@ -41,7 +48,7 @@ public class NaiveAccession {
 		userStore.establishUserSession(user, sessionKey);
 		
 		
-		Reddit reddit = new Reddit(user, userStore, Configuration.instance().getMessageStore());
+		Reddit reddit = new Reddit(user, userStore, CategorizeUs.instance().getMessageStore());
 		String after = null;
 		
 		do {
